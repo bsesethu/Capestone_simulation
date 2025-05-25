1d1e0f9be3e7617501928701fefc7bab49c809a1
Metse25 <metsemk@gmail.com>
2025-05-25 16:25:57 +0200

    Bar plotting redefined

--- data_analytics_scripts/Group1_data_analytics.py
+++ data_analytics_scripts/Group1_data_analytics.py
@@ -3,28 +3,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Reading the data from the CSV file.
+df = pd.read_csv("eastern_cape_gauteng_clean_data.csv")
+print(df)
+df = df[["CRIME CATEGORY", "Eastern Cape", "Gauteng"]]
+df = df.head(7)
 
-# Loading the cleaned data from CSV
-df_loaded = pd.read_csv("eastern_cape_gauteng_clean_data.csv")
-print("\nLoaded from CSV:")
-print(df_loaded)
+# Plotting
+plt.figure(figsize=(10, 6))
+bar_width = 0.35
+x = range(len(df))
 
-#plotting of graph (data visualisation)
+plt.bar(x, df["Eastern Cape"], width=bar_width, label="Eastern Cape", color='black')
+plt.bar([i + bar_width for i in x], df["Gauteng"], width=bar_width, label="Gauteng", color='grey')
 
-df_DataFrame = pd.melt(df_loaded, id_vars='CRIME CATEGORY',
-                    value_vars=['Eastern Cape', 'Gauteng'],
-                    var_name='Province', value_name='Crime Count')
-
+# Add labels and title
 print("\nSeaborn Barplot")
-plt.figure(figsize=(12, 8))
-sns.barplot(data=df_DataFrame, x='CRIME CATEGORY', y='Crime Count', hue='Province')
-plt.xticks(rotation=50, ha='right')
-plt.title("Crime Comparison: Eastern Cape vs Gauteng")
+plt.xlabel("Crime Category")
+plt.ylabel("Number of Cases")
+plt.title("Crime Statistics: Eastern Cape vs Gauteng")
+plt.xticks([i + bar_width / 2 for i in x], df["CRIME CATEGORY"], rotation=45, ha="right")
+plt.legend()
+plt.tight_layout()
 plt.show()
 
 
 #Analysing the data 
 #Descriptive Stats
-print("\nDescriptive Stats:")
-print(df_DataFrame.describe())
-
+# print("\nDescriptive Stats:")
+# print(df_DataFrame.describe())
